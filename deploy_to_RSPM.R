0ea783f5160504d5eb89e8ddbd86ae19a40ef546
Yohann Mansiaux <y.mansiaux@bordeaux-metropole.fr>
2021-10-04 14:57:37 +0200

    deploy rspm

---
+++ deploy_to_RSPM.R
@@ -0,0 +1,5 @@
+# sudo -s
+# alias rspm='/opt/rstudio-pm/bin/rspm'
+# rspm create git-builder --url=git@git.scnbdx.fr:UVD/datalab/packages-bm/shinyym.git --source=internal-gitlab-src --build-trigger=tags --ssh-key=bm-gitlab-ssh-key
+#
+# rspm run git-builder --name=shinyYM --source=internal-gitlab-src
